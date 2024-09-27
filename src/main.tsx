4b5efae4eaa286d952a5d060a8c596ab9756d241
aramsing <aramsin2@hotmail.com>
2024-09-27 15:30:48 -0500

    creating main page along with themes and starting points for website

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom';
+import React from 'react';
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import theme from "./theme.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function App() {
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <CssBaseline enableColorScheme />
+          <Routes>
+            <Route path="/" element={}></Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </React.StrictMode>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
