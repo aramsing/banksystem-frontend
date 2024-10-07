f2d7bc36c7663bec6da365a6b09193bdd9c9f6fb
aramsing <aramsin2@hotmail.com>
2024-10-07 14:23:30 -0500

    Home page fixed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export default function App() {
+    return (
+        <div>
+            <header className="font-bold text-4xl py-10 text-center">
+                <h1>BankSystem</h1>
+            </header>
+            <main className="h-screen">
+                <div className="text-2xl text-center">
+                    <label>Login to your account: </label>
+                    <button className="rounded-full cursor-pointer px-1">Login</button>
+                </div>
+            </main>
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
-//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import { StrictMode } from 'react'
+import App from './App.tsx'
 
-export default function App() {
-  return (
-    <div>
-      {/* header content */}
-      <header className="text-center">
-        <h1>Welcome to BankSystem</h1>
-      </header>
-
-      {/* main file content */}
-      <main>
-        <div>
-          <p className="text-center">Please choose from the options below</p>
-          <span className="inline-flex -space-x-px bg-white shadow-sm">
-            <button className="px-4 py-1 text-sm font-medium text-gray-700 hover:bg-gray-50">Log In</button>
-          </span>
-        </div>
-      </main>
-    </div>
-  );
-}
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
